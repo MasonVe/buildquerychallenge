34347c18998e0caae2961b273226cdc98ab065ca
MasonVe <101722004@student.swin.edu.au>
2020-08-10 09:16:29 +1000

    Task 1 Complete

--- buildquery.sql
+++ buildquery.sql
@@ -1,1 +1,18 @@
 --Mason Veljan 101722004
+/*
+TOUR (TourName, Description)
+PK (TourName)
+
+CLIENT (ClientID, Surname, GivenName, Gender)
+PK (ClientID)
+
+EVENT(TourName, EventYear, EventMonth, EventDay, Fee)
+PK (TourName, EventYear, EventMonth, EventDay)
+FK (TourName) References TOUR
+
+BOOKING(TourName, ClientID, EventYear, EventMonth, EventDay, DateBooked, Payment)
+PK (TourName, Client ID, EventYear, EventMonth, EventDay)
+FK (TourName, EventYear, EventMonth, EventDay, Fee) References EVENT
+FK (ClientID) References Client
+*/
+
